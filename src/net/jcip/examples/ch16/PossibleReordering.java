8d0119881b3966f9291068c0dc42d730ba76d14f
ncepuJelex <1170366657@qq.com>
2017-09-03 15:32:44 +0800

    chapter16 partial but full,the end!

---
+++ src/net/jcip/examples/ch16/PossibleReordering.java
@@ -0,0 +1,38 @@
+package net.jcip.examples.ch16;
+/**
+ * 你不确定会打印出什么来！
+ * 我这前几遍是（0，1），
+ * 但是也可能是(1,1) ,(0,0)...
+ * @author Jelex.xu
+ * @date 2017年9月3日
+ */
+public class PossibleReordering {
+
+	static int x = 0, y = 0;
+	static int a = 0, b = 0;
+	
+	public static void main(String [] args) throws InterruptedException {
+		
+		Thread one = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				a = 1;
+				x = b;
+			}
+		});
+		
+		Thread other = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				b = 1;
+				y = a;
+			}
+		});
+		
+		one.start();
+		other.start();
+		one.join();
+		other.join();
+		System.out.println("(" + x + "," + y + ")");
+	}
+}
